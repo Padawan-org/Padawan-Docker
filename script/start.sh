7bf15bd6e63a5a8857c0b6dc1c5828f604077ccc
Bruno de Lima e Silva <dlimaun@gmail.com>
2016-04-14 11:27:49 -0300

    Update start script. Add a command to remove server.pid

--- script/start.sh
+++ script/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+# Just call bundle install if needed
 bundle check || bundle install
-
+# Remove previous PID, when force close, it will remain on the tmp dir, we don't need it
+rm ./tmp/pids/server.pid
+# Exec rails
 bundle exec rails s -b 0.0.0.0 -p 3000
